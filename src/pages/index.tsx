c20ee985525140470a8d503d4b5b94b4cb2e2351
Murilo Wolfart <murilowolfart@hotmail.com>
2022-10-03 10:47:20 -0300

    [home]: adjust styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,11 @@ import { useTranslations } from 'use-intl'
 export default function Home(): ReactNode {
   const t = useTranslations('home')
   const description = t.rich('description', {
-    span: (children) => <span>{children}</span>,
+    span: (children) => <span className="text-primary">{children}</span>,
   })
   return (
-    <>
-      <div className="home-shape bg-dark flex flex-row py-24 pl-40 pr-24 mb-24">
+    <div className="h-screen">
+      <div className="home-shape bg-dark flex flex-row py-24 pl-40 pr-24 mb-32">
         <div className="pt-8 flex-grow">
           <span className="text-md text-lightblue">{t('prefix')}</span>
           <h1 className="flex-grow">{t('title')}</h1>
@@ -23,9 +23,9 @@ export default function Home(): ReactNode {
           />
         </div>
       </div>
-      <div className="pl-40 pr-24">
+      <div className="pl-40 pr-24 h-full">
         <p className="text-xl">{description}</p>
       </div>
-    </>
+    </div>
   )
 }
