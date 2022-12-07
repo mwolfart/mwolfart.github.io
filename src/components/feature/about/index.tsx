2f5a54f8aff6491fe7535d33792c2e7cfa4ee8e4
Murilo Wolfart <murilo.wolfart@materialplus.io>
2022-12-07 13:08:02 -0300

    [about; intro]: fix responsiveness

--- src/components/feature/about/index.tsx
+++ src/components/feature/about/index.tsx
@@ -4,16 +4,20 @@ import { Img } from '@ui-components'
 
 export const About: FC = () => {
   const t = useTranslations('about')
+  const Title = (
+    <h1 className="text-white border-b-2 border-lightblue w-fit pb-8">
+      {t('title')}
+    </h1>
+  )
   return (
     <div className="md:pr-24 md:py-12">
-      <div className="bg-dark p-8 flex flex-row gap-8">
-        <div className="p-4 border-2 border-primary h-fit">
+      <div className="bg-dark p-8 md:p-16 flex flex-col lg:flex-row gap-8">
+        <div className="lg:hidden self-center">{Title}</div>
+        <div className="p-4 my-4 border-2 border-primary self-center h-fit w-fit lg:self-start lg:my-0">
           <Img src="/images/profile-2x.jpg" width="w-60" height="h-72" />
         </div>
         <div className="flex flex-col gap-8">
-          <h1 className="text-white border-b-2 border-lightblue w-fit pb-8">
-            {t('title')}
-          </h1>
+          <div className="hidden lg:block">{Title}</div>
           <p className="text-md">{t('paragraphOne')}</p>
           <p className="text-md text-primary">{t('paragraphTwo')}</p>
           <p className="text-md">{t('paragraphThree')}</p>
--- src/components/feature/intro/index.tsx
+++ src/components/feature/intro/index.tsx
@@ -8,17 +8,21 @@ export const Intro: FC = () => {
     span: (children) => <span className="text-primary">{children}</span>,
   })
   return (
-    <div className="h-screen">
-      <div className="bg-dark flex flex-col items-center p-12 sm:pt-24 md:items-start md:flex-row md:home-title-shape lg:pl-40">
+    <div className="min-h-screen">
+      <div className="bg-dark flex flex-col items-center p-12 md:pt-24 md:items-start md:flex-row md:home-title-shape lg:pl-40">
         <div className="py-8 flex-grow text-center md:pb-0 sm:text-left">
           <span className="text-sm md:text-md text-lightblue">
             {t('prefix')}
           </span>
           <h1 className="flex-grow">{t('title')}</h1>
         </div>
-        <Img src="/images/computer.png" width="w-64" height="h-64" />
+        <Img
+          src="/images/computer.png"
+          width="w-32 sm:w-64"
+          height="h-32 sm:h-64"
+        />
       </div>
-      <div className="relative md:top-24 p-12 sm:p-24 lg:pl-40 md:w-2/3">
+      <div className="relative md:top-24 p-12 md:p-24 lg:pl-40 md:w-2/3">
         <p className="text-xl">{description}</p>
       </div>
     </div>
