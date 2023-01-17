895de5e90edd7d7dabb7f5e2c1d912adabcb5c43
Murilo Wolfart <murilo.wolfart@materialplus.io>
2023-01-17 16:21:14 -0300

    Don't limit project amount

--- src/components/feature/projects/index.tsx
+++ src/components/feature/projects/index.tsx
@@ -1,22 +1,13 @@
 import { forwardRef } from 'react'
 import { useTranslations } from 'use-intl'
 import { ProjectCard } from '@feature-components'
-import cx from 'classnames'
 
 export const Projects = forwardRef<HTMLDivElement>((_, ref) => {
   const t = useTranslations('projects')
   return (
     <div className="p-8 sm:p-16 flex flex-col gap-16" ref={ref}>
       <h2 className="pb-8 border-b-2 border-primary w-fit">{t('title')}</h2>
-      <div
-        className={cx(
-          'flex flex-row flex-wrap gap-8',
-          'lg:[&>*]:hidden',
-          'lg:[&>*:nth-child(-n+4)]:block',
-          '2xl:[&>*:nth-child(-n+6)]:block',
-          '3xl:[&>*:nth-child(-n+8)]:block',
-        )}
-      >
+      <div className="flex flex-row flex-wrap gap-8">
         <ProjectCard
           imageSrc="/images/emafy.jpg"
           title={t('emafy')}
