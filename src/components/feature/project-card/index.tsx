3a55939f6db22b342b68055ea8001f63ee44187d
Murilo Wolfart <murilo.wolfart@materialplus.io>
2023-02-01 09:50:04 -0300

    Experiment with focus trap

--- src/components/feature/project-card/index.tsx
+++ src/components/feature/project-card/index.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from 'react'
 import { Img } from '@ui-components'
 import cx from 'classnames'
+import { useTranslations } from 'use-intl'
 
 interface Props {
   imageSrc: string
@@ -15,9 +16,10 @@ export const ProjectCard: FC<Props> = ({
   description,
   onClick,
 }) => {
+  const t = useTranslations('projects')
   const [hasTransition, setTransition] = useState(true)
   const wrapperClasses = cx(
-    'bg-lightblue w-full md:w-half-gap-8 lg:w-96 cursor-pointer transition',
+    'text-left flex flex-col bg-lightblue w-full md:w-half-gap-8 lg:w-96 cursor-pointer transition',
     hasTransition && 'hover:scale-110',
   )
   const click = (): void => {
@@ -26,12 +28,16 @@ export const ProjectCard: FC<Props> = ({
     setTimeout(() => setTransition(true), 500)
   }
   return (
-    <div className={wrapperClasses} onClick={click}>
+    <button
+      className={wrapperClasses}
+      onClick={click}
+      aria-label={t('view-details')}
+    >
       <Img src={imageSrc} width="w-full" height="h-40" />
       <div className="p-4 flex flex-col gap-4">
         {title.length > 12 ? <h5>{title}</h5> : <h3>{title}</h3>}
         <p>{description}</p>
       </div>
-    </div>
+    </button>
   )
 }
--- src/components/feature/projects/details-dialog.tsx
+++ src/components/feature/projects/details-dialog.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@ui-components'
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useRef } from 'react'
 import { useTranslations } from 'use-intl'
 import cx from 'classnames'
+import FocusTrap from 'focus-trap-react'
 
 interface Props {
   content: ReactNode
@@ -15,18 +16,21 @@ export const ProjectDetailsDialog: FC<Props> = ({
   onClose,
 }) => {
   const t = useTranslations('projects')
+  const windowWidth = useRef(typeof window !== 'undefined' && window.innerWidth)
   const detailsWrapperClasses = cx(
-    'fixed md:absolute transition top-0 bottom-0 w-full bg-lightblue z-10 left-0 flex flex-col md:p-16',
+    'fixed md:absolute transition top-0 bottom-0 w-full bg-lightblue z-10 left-0 flex flex-col md:p-16 visible',
     !isOpen &&
-      'translate-x-[600px] sm:translate-x-[1200px] md:translate-x-[2000px]',
+      'translate-x-[600px] sm:translate-x-[1200px] md:translate-x-[2000px] invisible',
   )
 
   return (
-    <div className={detailsWrapperClasses}>
-      <div className="flex-grow p-10 md:p-0 overflow-y-auto">{content}</div>
-      <Button variant="outlined" className="mx-auto" onClick={onClose}>
-        {t('close')}
-      </Button>
-    </div>
+    <FocusTrap active={windowWidth && windowWidth.current < 768 && isOpen}>
+      <div className={detailsWrapperClasses}>
+        <div className="flex-grow p-10 md:p-0 overflow-y-auto">{content}</div>
+        <Button variant="outlined" className="mx-auto" onClick={onClose}>
+          {t('close')}
+        </Button>
+      </div>
+    </FocusTrap>
   )
 }
