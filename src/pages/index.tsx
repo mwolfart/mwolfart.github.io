2cc29bad72727a536263c7df338afe834d0a7cc0
Murilo Wolfart <murilo.wolfart@materialplus.io>
2023-06-01 21:54:01 -0300

    Fix favicon, page title and add strings to pt-br

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import { Intro, About, Projects, Contact } from '@feature-components'
 import { ScrollContext } from '@providers'
+import Head from 'next/head'
 import { useContext } from 'react'
 import { ReactNode } from 'react'
+import { useTranslations } from 'use-intl'
 
 export default function Home(): ReactNode {
   const { refs } = useContext(ScrollContext)
+  const t = useTranslations('header')
 
   return (
     <div className="h-full overflow-x-hidden">
+      <Head>
+        <title>{t('page-title')}</title>
+      </Head>
       <Intro ref={refs[0]} />
       <About ref={refs[1]} />
       <Projects ref={refs[2]} />
