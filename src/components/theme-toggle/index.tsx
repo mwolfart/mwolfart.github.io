a63c433a3a2c93f73070b9b30f83aa6797514b4b
Murilo Wolfart <murilo.wolfart@materialplus.io>
2024-06-17 11:18:30 -0300

    theme switcher

--- src/components/theme-toggle/index.tsx
+++ src/components/theme-toggle/index.tsx
@@ -1,12 +1,44 @@
+import { useLayoutEffect, useState } from "react";
+import { IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
+
 export const ThemeToggle = () => {
+  const prefersDark =
+    typeof window !== "undefined" &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setDark] = useState(prefersDark ?? false);
+
+  const breadcrumbStyle = `absolute h-[90%] top-[1.5px] left-[2px] transition bg-background aspect-square rounded-full ${
+    isDark ? "translate-x-0" : "translate-x-10"
+  }`;
+  const sunStyle = `absolute h-[70%] top-[4px] left-[6px] aspect-square ${
+    isDark ? "hidden" : "block"
+  }`;
+  const moonStyle = `absolute h-[70%] top-[4px] right-[7px] aspect-square ${
+    isDark ? "block" : "hidden"
+  }`;
+
+  useLayoutEffect(() => {
+    if (isDark && typeof document !== "undefined") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDark]);
+
   return (
     <button
-      className="bg-primary text-background rounded-full p-2"
+      className="relative rounded-full w-20 bg-copy text-background h-10 cursor-pointer"
       onClick={() => {
-        document.documentElement.classList.toggle("dark");
+        setDark(!isDark);
       }}
     >
-      Toggle theme
+      <div className={breadcrumbStyle} />
+      <div className={sunStyle}>
+        <IoSunnyOutline size={32} />
+      </div>
+      <div className={moonStyle}>
+        <IoMoonOutline size={32} />
+      </div>
     </button>
   );
 };
