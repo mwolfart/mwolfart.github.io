1d3496e6475e3ef2328d1bcc392a023f0619af2e
Murilo Wolfart <murilo.wolfart@materialplus.io>
2024-06-17 11:36:58 -0300

    scroller dots update

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,13 +1,22 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 type Props = {
   children: ReactNode[];
 };
 
 export const Carousel = ({ children }: Props) => {
+  const [activeItem, setActiveItem] = useState(0);
+
   return (
     <div className="flex flex-col gap-8">
-      <div className="flex gap-8 overflow-x-scroll snap-mandatory snap-x hide-scrollbar">
+      <div
+        className="flex gap-8 overflow-x-scroll snap-mandatory snap-x hide-scrollbar"
+        onScroll={(evt) => {
+          const target = evt.target as HTMLDivElement;
+          const index = Math.round(target.scrollLeft / target.clientWidth);
+          setActiveItem(index);
+        }}
+      >
         {children.map((child, idx) => (
           <div key={idx} className="shrink-0 basis-full snap-center">
             {child}
@@ -17,7 +26,12 @@ export const Carousel = ({ children }: Props) => {
       <div className="flex gap-2 justify-center items-center">
         <div className="flex-grow h-[1px] bg-copy" />
         {children.map((_, idx) => (
-          <div key={idx} className="h-2 bg-copy flex-grow rounded max-w-6" />
+          <div
+            key={idx}
+            className={`h-2 bg-copy flex-grow rounded max-w-6 ${
+              activeItem === idx && "bg-primary"
+            }`}
+          />
         ))}
         <div className="flex-grow h-[1px] bg-copy" />
       </div>
--- src/components/page-scroller/index.tsx
+++ src/components/page-scroller/index.tsx
@@ -1,18 +1,32 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 type Props = {
   children: ReactNode[];
 };
 
 export const PageScroller = ({ children }: Props) => {
+  const [activePage, setActivePage] = useState(0);
+
   return (
     <div className="h-full w-full relative">
       <div className="absolute left-4 top-0 bottom-0 flex flex-col justify-center items-center gap-2">
         {children.map((_, idx) => (
-          <div key={idx} className={`w-2 aspect-square rounded-full bg-copy`} />
+          <div
+            key={idx}
+            className={`w-2 aspect-square rounded-full bg-copy ${
+              activePage === idx && "bg-primary"
+            }`}
+          />
         ))}
       </div>
-      <div className="h-full overflow-y-auto overflow-x-hidden snap-mandatory snap-y hide-scrollbar">
+      <div
+        className="h-full overflow-y-auto overflow-x-hidden snap-mandatory snap-y hide-scrollbar"
+        onScroll={(evt) => {
+          const target = evt.target as HTMLDivElement;
+          const index = Math.round(target.scrollTop / target.clientHeight);
+          setActivePage(index);
+        }}
+      >
         {children.map((child, idx) => (
           <div
             key={idx}
