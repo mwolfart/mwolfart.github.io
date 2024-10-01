a695ddf0c014da273494c2b70da4a7cc3db7bc98
Murilo Wolfart <murilo.wolfart@materialplus.io>
2024-10-01 10:04:05 -0300

    feat: add extra languages

--- src/utils/i18n/config.ts
+++ src/utils/i18n/config.ts
@@ -1,21 +1,23 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import englishTranslation from "../../constants/content.json";
+import englishTranslation from "../../constants/translations/en.json";
+import portugueseTranslation from "../../constants/translations/pt.json";
 
-i18n
-    .use(initReactI18next)
-    .init({
-        lng: "en",
-        fallbackLng: "en",
-        debug: true,
-        interpolation: {
-            escapeValue: false,
-        },
-        resources: {
-            en: {
-                translation: englishTranslation,
-            },
-        },
-    });
-    
+i18n.use(initReactI18next).init({
+  lng: "en",
+  fallbackLng: "en",
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  },
+  resources: {
+    en: {
+      translation: englishTranslation,
+    },
+    pt: {
+      translation: portugueseTranslation,
+    },
+  },
+});
+
 export default i18n;
