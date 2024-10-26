ed183d914091c962aee72094af98027c114089b7
Murilo Wolfart <murilo.wolfart@materialplus.io>
2024-10-26 10:52:49 -0300

    fet: add cobalt ui description

--- src/components/page-projects/description-cobalt.tsx
+++ src/components/page-projects/description-cobalt.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { Carousel } from "../carousel";
 import { useMediaQuery } from "react-responsive";
 import { mediaQueries } from "../../constants/breakpoints";
@@ -7,15 +7,30 @@ export const Cobalt = () => {
   const { t } = useTranslation();
   const isScreenLg = useMediaQuery({ query: mediaQueries.lg });
 
+  const paragraphTwo = (
+    <Trans i18nKey="projects.cobalt.two">
+      The component library is publicly deployed to npm registry under the
+      identifier of @mwolfart/cobalt-ui and the storybook is avilable at
+      <a
+        href="https://mwolfart.github.io/cobalt-ui"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github Pages
+      </a>
+      .
+    </Trans>
+  );
+
   return (
     <Carousel negative>
       <div className="flex flex-col gap-4">
         <p>{t("projects.cobalt.one")}</p>
-        {isScreenLg && <p>{t("projects.cobalt.two")}</p>}
+        {isScreenLg && <p>{paragraphTwo}</p>}
       </div>
       {!isScreenLg && (
         <div className="flex flex-col gap-4">
-          <p>{t("projects.cobalt.two")}</p>
+          <p>{paragraphTwo}</p>
         </div>
       )}
     </Carousel>
