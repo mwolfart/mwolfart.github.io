4e7cb9006cd383b6cd937c73cee38d829c4fc3bc
Murilo Wolfart <murilo.wolfart@materialplus.io>
2025-02-20 17:46:54 -0300

    fix: change text blocks

--- src/components/page-intro/index.tsx
+++ src/components/page-intro/index.tsx
@@ -20,20 +20,17 @@ export const IntroPage = () => {
             <p>
               <Trans i18nKey="about.one">
                 Software developer with <span>7+</span> years of vast
-                experience, having worked with a variety of technologies ranging
-                from
-                <span>NextJS</span> and <span>React</span> to
-                <span>Golang</span>
-                and <span>MongoDB</span>.
+                experience, having worked with a variety of technologies
+                technologies including <span>NextJS</span>, <span>React</span>,
+                <span>Styled Components</span> and <span>Redux</span>
               </Trans>
             </p>
             <p>
               <Trans i18nKey="about.two">
                 I have worked in more than <span>10</span> different projects,
                 ranging from small websites to large-scale applications, and
-                have experience with <span>front-end</span>,
-                <span>back-end</span>,<span>databases</span> and
-                <span>DevOps</span>.
+                have experience not just with <span>front-end</span> but with
+                <span>back-end</span> as well.
               </Trans>
             </p>
             <p>
